611d8d5cba2fb2c212e2612346e78a90357eb21c
deradhista <deradhista@gmail.com>
2023-01-07 19:33:17 +0700

    Add: Alert fav, Hooks - feat: migration functional

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import Card from "../components/Card";
@@ -6,32 +6,20 @@ import Layout from "../components/Layout";
 import { LoadingAnimation, SkeletonLoading } from "../components/Loader";
 import Carousel from "../components/Carousel";
 import { MovieType } from "../utils/types/movie";
+import { useTitle } from "../utils/hooks/useTitle";
 
-interface PropsType {}
+const Index = () => {
+  useTitle("CINEMAX - Now Playing Movie");
+  const [datas, setDatas] = useState<MovieType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [page, setPage] = useState<number>(1);
+  const [totalPage, setTotalPage] = useState<number>(1);
 
-interface StateType {
-  loading: boolean;
-  datas: MovieType[];
-  page: number;
-  totalPage: number;
-}
+  useEffect(() => {
+    fetchData(1);
+  }, []);
 
-export default class index extends Component<PropsType, StateType> {
-  constructor(props: PropsType) {
-    super(props);
-    this.state = {
-      datas: [],
-      loading: true,
-      page: 1,
-      totalPage: 1,
-    };
-  }
-
-  componentDidMount() {
-    this.fetchData(1);
-  }
-
-  fetchData(page: number) {
+  function fetchData(page: number) {
     axios
       .get(
         `now_playing?api_key=${
@@ -41,101 +29,104 @@ export default class index extends Component<PropsType, StateType> {
       .then((data) => {
         const { results, total_pages } = data.data;
         //console.log(data);
-        this.setState({ datas: results, totalPage: total_pages });
+        setDatas(results);
+        setTotalPage(totalPage);
       })
       .catch((error) => {
         alert(error.toString());
       })
-      .finally(() => {
-        this.setState({ loading: false });
-      });
+      .finally(() => setLoading(false));
   }
 
-  nextPage() {
-    const newPage = this.state.page + 1;
-    this.setState({ page: newPage });
-    this.fetchData(newPage);
+  function nextPage() {
+    const newPage = page + 1;
+    setPage(newPage);
+    fetchData(newPage);
   }
 
-  prevPage() {
-    const newPage = this.state.page - 1;
-    this.setState({ page: newPage });
-    this.fetchData(newPage);
+  function prevPage() {
+    const newPage = page - 1;
+    setPage(newPage);
+    fetchData(newPage);
   }
 
-  handleFavorite(data: MovieType) {
-    const checkExist = localStorage.getItem("FavMovie");
-    console.log(checkExist);
-    if (checkExist) {
-      let parseFav: MovieType[] = JSON.parse(checkExist);
-      parseFav.push(data);
-      localStorage.setItem("FavMovie", JSON.stringify(parseFav));
+  function handleFavorite(data: MovieType) {
+    const getMovies = localStorage.getItem("FavMovie");
+
+    if (getMovies) {
+      let parseFav: MovieType[] = JSON.parse(getMovies);
+      const movieExist = parseFav.find((item) => item.id === data.id);
+
+      if (movieExist) {
+        alert("Movie already on Favorite");
+      } else {
+        parseFav.push(data);
+        localStorage.setItem("FavMovie", JSON.stringify(parseFav));
+      }
     } else {
       localStorage.setItem("FavMovie", JSON.stringify([data]));
     }
     alert("Movie added to Favorite");
   }
 
-  render() {
-    return (
-      <Layout>
-        {!this.state.loading && (
-          <Carousel
-            datas={this.state.datas.slice(0, 5)}
-            content={(data) => (
-              <div
-                className="w-full h-full flex justify-center items-center bg-cover bg-center"
-                style={{
-                  backgroundImage: `linear-gradient(
+  return (
+    <Layout>
+      {!loading && (
+        <Carousel
+          datas={datas.slice(0, 5)}
+          content={(data) => (
+            <div
+              className="w-full h-full flex justify-center items-center bg-cover bg-center"
+              style={{
+                backgroundImage: `linear-gradient(
                     rgba(0, 0, 0, 0.5),
                     rgba(0, 0, 0, 0.5)
                   ), url(https://image.tmdb.org/t/p/original${data.poster_path})`,
-                }}
-              >
-                <p className="text-white tracking-widest font-bold break-words text-2xl">
-                  {data.title}
-                </p>
-              </div>
-            )}
-          />
-        )}
-        <div className="mx-12 pt-10 pb-5">
-          <h2 className="font-extrabold text-2xl text-center">NOW PLAYING</h2>
-        </div>
-        <div className="grid grid-cols-4 gap-3">
-          {this.state.loading
-            ? [...Array(20).keys()].map((data) => (
-                <LoadingAnimation key={data} />
-              ))
-            : this.state.datas.map((data: MovieType) => (
-                <Card
-                  key={data.id}
-                  title={data.title}
-                  image={data.poster_path}
-                  id={data.id}
-                  labelButton="ADD TO FAVORITE"
-                  onClickFav={() => this.handleFavorite(data)}
-                />
-              ))}
-        </div>
-        <div className="btn-group w-full justify-center pt-10">
-          <button
-            className="btn"
-            onClick={() => this.prevPage()}
-            disabled={this.state.page === 1}
-          >
-            «
-          </button>
-          <button className="btn">{this.state.page}</button>
-          <button
-            className="btn"
-            onClick={() => this.nextPage()}
-            disabled={this.state.page === this.state.totalPage}
-          >
-            »
-          </button>
-        </div>
-      </Layout>
-    );
-  }
-}
+              }}
+            >
+              <p className="text-white tracking-widest font-bold break-words text-2xl">
+                {data.title}
+              </p>
+            </div>
+          )}
+        />
+      )}
+      <div className="mx-12 pt-10 pb-5">
+        <h2 className="font-extrabold text-2xl text-center">NOW PLAYING</h2>
+      </div>
+      <div className="grid grid-cols-4 gap-3">
+        {loading
+          ? [...Array(20).keys()].map((data) => <LoadingAnimation key={data} />)
+          : datas.map((data: MovieType) => (
+              <Card
+                key={data.id}
+                title={data.title}
+                image={data.poster_path}
+                id={data.id}
+                labelButton="ADD TO FAVORITE"
+                onClickFav={() => handleFavorite(data)}
+              />
+            ))}
+      </div>
+      <div className="btn-group w-full justify-center pt-10">
+        <button
+          className="btn"
+          onClick={() => prevPage()}
+          disabled={page === 1}
+        >
+          «
+        </button>
+        <button className="btn">{page}</button>
+        <button
+          className="btn"
+          onClick={() => nextPage()}
+          disabled={page === totalPage}
+        >
+          »
+        </button>
+      </div>
+    </Layout>
+  );
+};
+
+export default Index;
